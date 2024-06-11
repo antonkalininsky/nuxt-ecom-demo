03f879b961255c3dd73a27d5ad72d4ba8b340dbd
anton <chadik.free@gmail.com>
2024-06-11 12:51:04 +0500

    nav: layout in work

--- app.vue
+++ app.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div>
     <!-- header -->
     <div
-      class="flex justify-between h-20 items-center px-10 text-sm font-bold border-b-2"
+      class="flex justify-between h-20 items-center px-10 text-sm font-bold border-b-2 w-100"
     >
       <div class="flex items-center">
-        <div class="pr-6">
+        <div class="pr-12 cursor-pointer">
           <img src="/images/logo.svg" alt="gamepay" class="w-48" />
         </div>
-        <button class="w-10 h-10 flex justify-center items-center">
-          <MdiIcon icon="mdiMenu" :size="'100'" view-box="0 0 24 24" />
+        <button
+          class="w-20 h-20 flex justify-center items-center border-x-2 hover:bg-red-500 hover:text-white transition"
+        >
+          <Icon name="material-symbols-light:menu" size="2rem" />
         </button>
         <nav>
           <ul class="flex px-6">
@@ -31,10 +33,23 @@
           </ul>
         </nav>
       </div>
-      <div>ПОИСК ИГР</div>
+      <div
+        class="flex gap-x-3 items-center hover:text-red-500 cursor-pointer leading-10 transition"
+      >
+        <Icon name="material-symbols-light:search" size="2rem" color="red" />
+        <div>ПОИСК ИГР</div>
+      </div>
       <div class="flex">
-        <div>money</div>
-        <div>cart</div>
+        <div
+          class="cursor-pointer hover:bg-red-500 hover:text-white w-20 h-20 flex justify-center items-center"
+        >
+          <Icon name="ph:currency-rub-light" size="2rem" />
+        </div>
+        <div
+          class="cursor-pointer hover:bg-red-500 hover:text-white w-20 h-20 flex justify-center items-center"
+        >
+          <Icon name="ion:cart-outline" size="2rem" />
+        </div>
       </div>
     </div>
   </div>
